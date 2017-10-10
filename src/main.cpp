859b8ecd10f1a413f20b7816c7574e32bc1748ed
Henry <hacoo36@gmail.com>
2017-10-10 14:20:05 -0700

    CLI parsing now uses boost program_options

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,9 @@
 #include <cstdlib>
 #include <cctype>
 #include <unistd.h>
+#include <cstdio>
+#include <boost/program_options.hpp>
+#include <stdexcept>
 
 #include "Client.h"
 #include "TimeManager.h"
@@ -32,13 +35,12 @@ Client client(manager, zobrist, tt);
 std::atomic<bool> currently_pondering_atom;
 std::atomic<bool> continue_pondering_atom;
 
-// Print usage message and exit
+// Print usage message
 void usage() {
   printf("usage: dougchess [-p port] [-n name] \n");
   printf(" p port: port to connect to minichess framework (default 54361) \n");
   printf(" n name: name to register with framework (default Doug) \n");
   printf(" example: dougchess -p 12345 -n Example \n");
-  exit(1);
 }
 
 void main_sigint(int signum) {
@@ -58,37 +60,50 @@ int main(int argc, char* argv[]) {
   signal(SIGINT, main_sigint);
   
   tt.clear();
-  
-  int port = 54361;
-  char* name = NULL;
-  int c;
-  while ((c = getopt(argc, argv, "p:n:")) != -1) {
-    switch(c) {
-    case 'p':
-      port = atoi(optarg);
-      break;
-    case 'n':
-      name = optarg;
-      break;
-    case '?':
+
+  try {
+
+    namespace bpo = boost::program_options;
+
+    bpo::options_description desc{"Options"};
+    desc.add_options()
+      ("help,h", "Show help")
+      ("port,p", bpo::value<int>()->default_value(54361), "Port on which to connect to framework")
+      ("name,n", bpo::value<std::string>()->default_value("Doug"), "AI name");
+
+
+    bpo::variables_map vm;
+    bpo::store(bpo::parse_command_line(argc, argv, desc), vm);
+    bpo::notify(vm);
+
+    int port = 0;
+    std::string name;
+
+    if (vm.count("help")) {
       usage();
-    default:
-      usage();
+      return 1;
     }
-  }
 
-  if (name == NULL) { 
-    printf("Connecting as Doug on port %d...\n", port);
-    client.start(port, "Doug");
-  } else {
-    printf("Connecting as %s on port %d...\n", name, port);
+    if (vm.count("port")) {
+      port = vm["port"].as<int>();
+    }
+
+    if (vm.count("name")) {
+      name = vm["name"].as<std::string>();
+    }
+
+    printf("Connecting as %s on port %d...\n", name.c_str(), port);
     client.start(port, name);
+
+    printf("Disconnecting...\n");
+    client.disconnect();
+    printf("Disconnected OK!\n");
+
+  } catch (const std::exception& ex) {
+    std::cerr << ex.what() << std::endl;
+    return 1;
   }
 
-  printf("Disconnecting...\n");
-  client.disconnect();
-  printf("Disconnected OK!\n");
-  
   return 0;
 }
 
